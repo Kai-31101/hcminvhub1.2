821916d62d9add2baf5cc7ae73986a0d6775aadc
Kai-31101 <161666442+Kai-31101@users.noreply.github.com>
2026-05-21 17:13:49 +0700

    Use provided Arobid footer logo

--- src/app/components/ArobidLogo.tsx
+++ src/app/components/ArobidLogo.tsx
@@ -2,36 +2,10 @@ import React from 'react';
 
 export function ArobidLogo({ className = '' }: { className?: string }) {
   return (
-    <svg
+    <img
+      src="/figma-homepage/arobid-logo.svg"
       className={className}
-      viewBox="0 0 360 94"
-      role="img"
-      aria-label="arobid.com - A Road to Big Deals"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path d="M0 70 38 0l40 70H0Z" fill="#f56600" />
-      <path d="M10 63 28 39l15 17L70 15 42 70 28 52 10 63Z" fill="#fff" />
-      <text
-        x="88"
-        y="54"
-        fill="#020617"
-        fontFamily="Arial, Helvetica, sans-serif"
-        fontSize="44"
-        fontWeight="800"
-        letterSpacing="-2"
-      >
-        arobid.com
-      </text>
-      <text
-        x="139"
-        y="84"
-        fill="#020617"
-        fontFamily="Arial, Helvetica, sans-serif"
-        fontSize="22"
-        fontWeight="700"
-      >
-        A Road to Big Deals
-      </text>
-    </svg>
+      alt="arobid.com - A Road to Big Deals"
+    />
   );
 }
